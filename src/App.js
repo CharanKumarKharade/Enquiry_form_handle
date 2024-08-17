4dd4809602c863b9b05755f051dcc51cf9313914
unknown <CharanKumarKharadeS>
2024-08-17 13:32:15 +0530

    enquiry form done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [formData, setformData] = useState({
+    uname: "",
+    uemail: "",
+    uphone: "",
+    index: "",
+  });
+  let [userData, setuserData] = useState([]);
+  let getValue = (event) => {
+    let oldData = { ...formData };
+    let inputName = event.target.name;
+    oldData[inputName] = event.target.value;
+    setformData(oldData);
+  };
+  let handleSubmit = (event) => {
+    let currentData = {
+      name: formData.uname,
+      email: formData.uemail,
+      phone: formData.uphone,
+      index: formData.index,
+    };
+    let oldUserData = [...userData, currentData];
+    setuserData(oldUserData);
+    event.preventDefault();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="form-data">
+        <h2>Enquiry Details</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-elements">
+            <label>Name </label>
+            <br />
+            <input
+              type="text"
+              onChange={getValue}
+              name="uname"
+              value={formData.uname}
+            />
+          </div>
+          <div className="form-elements">
+            <label>Email </label>
+            <br />
+            <input
+              type="email"
+              onChange={getValue}
+              name="uemail"
+              value={formData.uemail}
+            />
+          </div>
+          <div className="form-elements">
+            <label>Phone Number </label>
+            <input
+              type="text"
+              onChange={getValue}
+              name="uphone"
+              value={formData.uphone}
+            />
+          </div>
+
+          <button>{formData.index !== "" ? "Update" : "Save"}</button>
+        </form>
+      </div>
+    </>
   );
 }
 
